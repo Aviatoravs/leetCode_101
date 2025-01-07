5159628347f73fe079a62cb91f18c2ff28ac0f3a
Aviral <aviatoravs@gmail.com>
2025-01-07 15:58:12 +0530

    little changes

--- PalindromeNumber.py
+++ PalindromeNumber.py
@@ -1,9 +1,11 @@
 x = int(input("Enter a Number to check if it is palindrome or not : "))
-temp = abs(x)
+temp = x
 rev = 0
 while(temp > 0):
     temp0 = temp%10
     rev = rev*10 + temp0
     temp = temp//10
-if(rev == abs(x)):
+if(rev == x):
     print("This Number is palindrome")
+else:
+    print("Think of a funny Thing")
