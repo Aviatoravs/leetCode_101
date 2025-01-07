a7311848277a127426eeca271fd040d2ea2a5651
Aviral <aviatoravs@gmail.com>
2025-01-07 15:00:31 +0530

    Easy Problem - 2

---
+++ PalindromeNumber.py
@@ -0,0 +1,9 @@
+x = int(input("Enter a Number to check if it is palindrome or not : "))
+temp = x
+rev = 0
+while(temp > 0):
+    temp0 = temp%10
+    rev = rev*10 + temp0
+    temp = temp//10
+if(rev == x):
+    print("This Number is palindrome")
